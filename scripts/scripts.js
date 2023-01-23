70b3687ab9deb3e69335eb7f01afa30fe5e3c58b
LucasDCoelho <94465173+LucasDCoelho@users.noreply.github.com>
2023-01-22 23:57:24 -0300

    finished: exer.: 01, 02, 03 and 04

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,44 @@
 "use strict";
-
-
-const pushOfBody = document.getElementById('user');
-let nome = prompt("Qual seu nome??");
-let age = {
-    age: prompt("Qual sua Idade?")
-
+const userId = document.getElementById('user');
+const userAgeId = document.getElementById('user_age');
+const alunos = document.getElementById('alunos');
+// let nome = prompt("Digite seu nome?" as string) as string;
+// let ano = prompt(`${nome} em que ano você nasceu?`)
+// function welcome() {
+//   userId.innerHTML = `Seja bem-vindo ${nome}`;
+// }
+// function welcomeWithAge(age: number) {
+//   age = 2023 - (Number(ano))
+//   userAgeId.innerHTML = `Prazer :3 ${nome}. Sua idade é de ${age - 1} anos`;
+// }
+// Retorna os que faltaram
+let maxClassmates = 20;
+let activeClassmates = 19;
+let rainDay = true;
+function percentualClassmates(alunosChuva) {
+    let resultDosQueVieram = (alunosChuva * 100) / maxClassmates;
+    let resultDosQueNaoVieram = 100 - resultDosQueVieram;
+    return alunos.innerHTML = String(`${resultDosQueNaoVieram}% da turma faltou T....T`);
 }
-
-function welcome(){
-    pushOfBody.innerHTML = `Seja bem vindo ${nome}`
+;
+function media(teo, prat1, prat2) {
+    teo = teo * 2;
+    return (teo + prat1 + prat2) / 3;
 }
-
-function age(age){
-    pushOfBody.innerHTML = `Seja bem vindo ${nome}. E sua idade é ${age}`
+let manha = new Date('0 05:00');
+let tarde = new Date('0 12:00');
+let noite = new Date('0 18:00');
+let manhaEnd = new Date('0 11:59');
+console.log(manhaEnd.getTime());
+function parkTime(time) {
+    if (time >= 5 && time <= 11.59) {
+        console.log("Manhã");
+    }
+    else if (time >= 12 && time <= 17.59) {
+        console.log("Tarde");
+    }
+    else if (time >= 18 && time <= 4.59) {
+        console.log("Noite");
+    }
 }
-
-welcome();
+console.log(Date.now());
--- scripts/scripts.ts
+++ scripts/scripts.ts
@@ -1,11 +1,60 @@
+const userId = document.getElementById('user') as HTMLDivElement;
+const userAgeId = document.getElementById('user_age') as HTMLDivElement;
+const alunos = document.getElementById('alunos') as HTMLDivElement;
+// let nome = prompt("Digite seu nome?" as string) as string;
+// let ano = prompt(`${nome} em que ano você nasceu?`)
+
+// function welcome() {
+//   userId.innerHTML = `Seja bem-vindo ${nome}`;
+// }
+
+// function welcomeWithAge(age: number) {
+//   age = 2023 - (Number(ano))
+//   userAgeId.innerHTML = `Prazer :3 ${nome}. Sua idade é de ${age - 1} anos`;
+// }
 
 
-function greeter(fn: (a: string) => void) {
-    fn("Hello, World");
+// Retorna os que faltaram
+let maxClassmates = 20;
+let activeClassmates = 19;
+let rainDay = true;
+
+function percentualClassmates( alunosChuva: number){
+  let resultDosQueVieram = (alunosChuva * 100) / maxClassmates;
+  let resultDosQueNaoVieram = 100 - resultDosQueVieram ;
+  return  alunos.innerHTML= String(`${resultDosQueNaoVieram}% da turma faltou T....T`);
+};
+
+
+
+function media(teo: number, prat1: number, prat2: number){
+  teo = teo * 2;
+  return (teo + prat1 + prat2) / 3
+}
+
+
+let manha = new Date('0 05:00');
+let tarde =  new Date('0 12:00');
+let noite =  new Date('0 18:00');
+
+let manhaEnd = new Date('0 11:59')
+
+
+console.log(manhaEnd.getTime())
+
+
+
+function parkTime(time: number){
+  
+
+  if(time >= 5 && time <= 11.59){
+    console.log("Manhã")
+  } else if(time >= 12 && time <= 17.59){
+    console.log("Tarde")
+  } else if(time >= 18 && time <= 4.59){
+    console.log("Noite")
   }
-   
-  function printToConsole(s: string) {
-    console.log(s);
-  }
-   
-  greeter(printToConsole);
+
+}
+
+console.log(Date.now())
